a7b52d828ff9f7f4bca29709a34e3928b35edafe
ishara <ishara@mtanium.com>
2020-11-17 19:03:52 +0100

    fixed errors

--- api/dataSyncFunction/common-util.js
+++ api/dataSyncFunction/common-util.js
@@ -1,16 +1,16 @@
-const winston = require('winston');
+const winston = require("winston");
 
 const logger = new winston.createLogger({
-    level: 'info',
-    exitOnError: false,
-    transports: [
-        new winston.transports.Console({
-            prettyPrint: true,
-            colorize: true,
-        }),
-    ],
+  level: "info",
+  exitOnError: false,
+  transports: [
+    new winston.transports.Console({
+      prettyPrint: true,
+      colorize: true,
+    }),
+  ],
 });
 
 module.exports = {
-    logger: logger,
+  logger: logger,
 };
--- api/dataSyncFunction/data-sync.js
+++ api/dataSyncFunction/data-sync.js
@@ -1,12 +1,9 @@
-'use strict';
-const resposeUtil = require('./response-util');
-const { logger } = require('./common-util');
-const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+"use strict";
+const resposeUtil = require("./response-util");
+const { logger } = require("./common-util");
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 exports.syncData = async (event) => {
-
-    logger.info("Inside sync data function");
-
-
-}
+  logger.info("Inside sync data function");
+};
